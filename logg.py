6f5649e839ed0ccb9688c604954f3e3bad95960d
Natalia Cherniavskaia <natatata931@mail.ru>
2023-02-01 15:08:55 +0300

    gitignore

--- logg.py
+++ logg.py
@@ -1,10 +1,13 @@
 import logging
+import datetime
+
+logging.basicConfig(level=logging.INFO, filename="py_log.log")
 
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    encoding='utf-8',
-    filename="log_file.log",
-    format="%(asctime)s -- %(module)s -- %(levelname)s -- %(message)s",
-    datefmt='%d_%m_%Y %H:%M:%S',
-)
+def lod_action(mm):
+    now = datetime.datetime.now()
+    logging.info(f"{now} {mm}")
+
+def log_warn(mm):
+    now = datetime.datetime.now()
+    logging.warning(f"{now} {mm}")
--- sql.py
+++ sql.py
@@ -1,11 +1,7 @@
 import sqlite3
-import logg
-import ui
 
 conn = sqlite3.connect('hagwarts.db')
 cur = conn.cursor()
-
-
 
 cur.execute("""CREATE TABLE IF NOT EXISTS students(
    id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,11 +22,7 @@ conn.commit()
 
 
 def add(name, surname, gender, faculty):
-    logg. logging.info(
-        f"Добавление студента {name} {surname} {gender} {faculty}")
     cur.execute("INSERT INTO students (name, surname, gender, faculty) VALUES (?,?,?,?);",
                 (name, surname, gender, faculty))
     conn.commit()
-    print(f"Студент {name} {surname} {gender} {faculty} успешно добавлен!\n\n\n")
-    ui.menu()
 
